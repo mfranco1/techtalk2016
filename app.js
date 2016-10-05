c37b55a4b5058f614a2c73bfefa339c28e9a44b1
Marcelino Franco III <marcelino.francoiii@factset.com>
2016-10-05 04:54:39 +0100

    this version allows tweets to be made

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Twitter = require('twitter');
+var config = require('./config.js');
 
 var ipaddress = '';
 require('dns').lookup(require('os').hostname(), function(err, addr, fam){
   console.log('ip address local: ' + addr);
   ipaddress = addr;
+});
+
+var client = new Twitter({
+  consumer_key: config.twitter.CONSUMER_KEY,
+  consumer_secret: config.twitter.CONSUMER_SECRET,
+  access_token_key: config.twitter.ACCESS_TOKEN_KEY,
+  access_token_secret: config.twitter.ACCESS_TOKEN_SECRET
 });
 
 app.get('/', function(req,res){
@@ -18,7 +27,10 @@ io.on('connection', function(socket){
 
   socket.on('new_filter', function(phrase){
     console.log('filter keyword: ' + phrase);
-    io.emit('stream_data', 'filter keyword: ' + phrase);
+    client.post('statuses/update', {status: 'testing twitter API: ' + phrase}, function(err, tweet, res){
+      if(err) throw err;
+      console.log(tweet);
+    });
   });
 });
 
